8905122a00d42230a1a9b619cf97d21593f27d59
Patrícia <sociaisredes.pa@gmail.com>
2026-05-17 01:00:06 -0300

    Estilização Nova [1]

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Site funcionando!");
+console.log("Portfólio carregado com sucesso!");
+
+/* ANIMAÇÃO NOS CARDS */
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+
+    card.addEventListener("mouseenter", () => {
+
+        card.style.transform =
+        "translateY(-10px) scale(1.03)";
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform =
+        "translateY(0px) scale(1)";
+    });
+
+});
